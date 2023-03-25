2547eee3c4a8a4be031c8fdf28a8fd52f4f6fb1c
Lewis McCormack <2801934m@student.gla.ac.uk>
2023-03-25 14:23:43 +0000

    Chapter 4 completed. Committed using git add tango_with_django_project/

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -13,4 +13,6 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    context_dict = {'boldmessage': 'This tutorial has been put together by Lewis McCormack.'}
+    # The above line does the same as it does in index view.
+    return render(request, 'rango/about.html', context=context_dict)
